290c7592a574ea3703076473db4453517b189d66
Kent-Danielle <kent.concengco.acads@gmail.com>
2026-05-31 13:50:33 -0700

    feat(app): register share-link route

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ import { AppLayout } from "./components/layout/AppLayout";
 import { WelcomePage } from "@/pages/WelcomePage";
 import { CreateSessionPage } from "@/pages/CreateSession/CreateSessionPage";
 import { JoinSessionPage } from "@/pages/JoinSession/JoinSessionPage";
-import { CREATE_SESSION_ROUTE, JOIN_SESSION_ROUTE, JOIN_SESSION_WITH_LINK_ROUTE } from "@/common/routes";
+import { ShareJoinLinkPage } from "@/pages/ShareLink/ShareJoinLinkPage";
+import {
+	CREATE_SESSION_ROUTE,
+	JOIN_SESSION_ROUTE,
+	JOIN_SESSION_WITH_LINK_ROUTE,
+	SHARE_JOIN_LINK_ROUTE,
+} from "@/common/routes";
 
 function App() {
 	return (
@@ -14,6 +20,7 @@ function App() {
 					<Route path={CREATE_SESSION_ROUTE} element={<CreateSessionPage />} />
 					<Route path={JOIN_SESSION_ROUTE} element={<JoinSessionPage />} />
 					<Route path={JOIN_SESSION_WITH_LINK_ROUTE} element={<JoinSessionPage />} />
+					<Route path={SHARE_JOIN_LINK_ROUTE} element={<ShareJoinLinkPage />} />
 					<Route path="*" element={<WelcomePage />} />
 				</Routes>
 			</AppLayout>
