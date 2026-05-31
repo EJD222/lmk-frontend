851db9ec25e68c357ccf17ef7248bebbaa6a4ca1
Kent-Danielle <kent.concengco.acads@gmail.com>
2026-05-30 23:25:23 -0700

    chore(routing): add JOIN_SESSION_WITH_LINK_ROUTE constant

--- src/common/routes.tsx
+++ src/common/routes.tsx
@@ -1,2 +1,3 @@
 export const CREATE_SESSION_ROUTE: string = "/create-session";
 export const JOIN_SESSION_ROUTE: string = "/join-session";
+export const JOIN_SESSION_WITH_LINK_ROUTE: string = "/join-session/:linkId";
