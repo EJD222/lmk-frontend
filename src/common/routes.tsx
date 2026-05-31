f6c028f0f2ae349cd5a115a7e6762fff25a53b13
Kent-Danielle <kent.concengco.acads@gmail.com>
2026-05-31 13:50:32 -0700

    chore(routing): add SHARE_JOIN_LINK_ROUTE constant

--- src/common/routes.tsx
+++ src/common/routes.tsx
@@ -1,3 +1,4 @@
 export const CREATE_SESSION_ROUTE: string = "/create-session";
 export const JOIN_SESSION_ROUTE: string = "/join-session";
 export const JOIN_SESSION_WITH_LINK_ROUTE: string = "/join-session/:linkId";
+export const SHARE_JOIN_LINK_ROUTE: string = "/share-link";
