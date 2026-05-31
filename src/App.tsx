be6b47f6de6e4c9950b7ce1a5e191034d775168d
Kent-Danielle <kent.concengco.acads@gmail.com>
2026-05-30 18:43:46 -0700

    feat(app): register create-session route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AppLayout } from "./components/layout/AppLayout";
 import { WelcomePage } from "@/pages/WelcomePage";
+import { CREATE_SESSION_ROUTE } from "@/common/routes";
+import { CreateSessionPage } from "@/pages/CreateSession/CreateSessionPage";
 
 function App() {
 	return (
@@ -8,6 +10,7 @@ function App() {
 			<AppLayout>
 				<Routes>
 					<Route path="/" element={<WelcomePage />} />
+                    <Route path={CREATE_SESSION_ROUTE} element={<CreateSessionPage />} />
 					<Route path="*" element={<WelcomePage />} />
 				</Routes>
 			</AppLayout>
