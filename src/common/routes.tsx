1583188648c04b936670f763bacaa639a4b7526a
Kent-Danielle <kent.concengco.acads@gmail.com>
2026-06-01 00:21:42 -0700

    chore(routing): add RESULTS_ROUTE

--- src/common/routes.tsx
+++ src/common/routes.tsx
@@ -3,6 +3,7 @@ export const JOIN_SESSION_ROUTE: string = "/join-session";
 export const JOIN_SESSION_WITH_LINK_ROUTE: string = "/join-session/:linkId";
 export const SHARE_JOIN_LINK_ROUTE: string = "/share-link";
 export const SESSION_ROUTE: string = "/session";
+export const RESULTS_ROUTE: string = "/results";
 
 export function buildSessionPath(_sessionId: string): string {
   return "/session";
