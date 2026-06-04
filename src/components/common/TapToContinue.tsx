50ca99c87e69a8b8782c899220b70c42460980d6
Kent-Danielle <kent.concengco.acads@gmail.com>
2026-06-03 23:51:38 -0700

    fix(common): fix TapToContinue centering

--- src/components/common/TapToContinue.tsx
+++ src/components/common/TapToContinue.tsx
@@ -8,7 +8,7 @@ export function TapToContinue({ className }: TapToContinueProps) {
   return (
     <div
       className={cn(
-        "absolute text-xs -translate-x-1/2 opacity-40 bottom-8 left-1/2 animate-bounce",
+        "absolute text-xs left-0 right-0 text-center opacity-40 bottom-8 animate-bounce",
         className
       )}
     >
