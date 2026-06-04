98a92b6ea8db0c2ba312dc5290a812bba9889714
Kent-Danielle <kent.concengco.acads@gmail.com>
2026-06-03 22:35:30 -0700

    refactor(app): remove mock result pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { CreateSessionPage } from "@/pages/CreateSession/CreateSessionPage";
 import { JoinSessionPage } from "@/pages/JoinSession/JoinSessionPage";
 import { ShareJoinLinkPage } from "@/pages/ShareLink/ShareJoinLinkPage";
 import { SessionPage } from "@/pages/Session/SessionPage";
+import { ResultsPage } from "@/pages/Results/ResultsPage";
 import {
   CREATE_SESSION_ROUTE,
   JOIN_SESSION_ROUTE,
@@ -12,26 +13,7 @@ import {
   SHARE_JOIN_LINK_ROUTE,
   SESSION_ROUTE,
   RESULTS_ROUTE,
-  CONSENSUS_RESULT_ROUTE,
-  SPLIT_RESULT_ROUTE,
-  BY_THE_NUMBERS_RESULT_ROUTE,
-  AI_RECOMMENDATION_RESULT_ROUTE,
-  ALSO_ON_TABLE_RESULT_ROUTE,
-  SHARE_RESULTS_ROUTE,
-  NO_CONSENSUS_RESULT_ROUTE,
-  NO_CONSENSUS_DIVERGED_ROUTE,
-  NO_CONSENSUS_WHAT_NOW_ROUTE,
 } from "@/common/routes";
-import { ResultsPage } from "./pages/Results/ResultsPage";
-import { ConsensusPage } from "./pages/Results/ConsensusPage";
-import { SplitRevealPage } from "./pages/Results/SplitRevealPage";
-import { ByTheNumbersPage } from "./pages/Results/ByTheNumbersPage";
-import { AIRecommendationPage } from "./pages/Results/AIRecommendationPage";
-import { AlsoOnTheTablePage } from "./pages/Results/AlsoOnTheTablePage";
-import { ShareResultsPage } from "./pages/Results/ShareResultsPage";
-import { NoConsensusIntroPage } from "./pages/Results/NoConsensusIntroPage";
-import { NoConsensusDivergedPage } from "./pages/Results/NoConsensusDivergedPage";
-import { NoConsensusWhatNowPage } from "./pages/Results/NoConsensusWhatNowPage";
 
 function App() {
   return (
@@ -45,15 +27,6 @@ function App() {
           <Route path={SHARE_JOIN_LINK_ROUTE} element={<ShareJoinLinkPage />} />
           <Route path={SESSION_ROUTE} element={<SessionPage />} />
           <Route path={RESULTS_ROUTE} element={<ResultsPage />} />
-          <Route path={CONSENSUS_RESULT_ROUTE} element={<ConsensusPage />} />
-          <Route path={SPLIT_RESULT_ROUTE} element={<SplitRevealPage />} />
-          <Route path={BY_THE_NUMBERS_RESULT_ROUTE} element={<ByTheNumbersPage />} />
-          <Route path={AI_RECOMMENDATION_RESULT_ROUTE} element={<AIRecommendationPage />} />
-          <Route path={ALSO_ON_TABLE_RESULT_ROUTE} element={<AlsoOnTheTablePage />} />
-          <Route path={SHARE_RESULTS_ROUTE} element={<ShareResultsPage />} />
-          <Route path={NO_CONSENSUS_RESULT_ROUTE} element={<NoConsensusIntroPage />} />
-          <Route path={NO_CONSENSUS_DIVERGED_ROUTE} element={<NoConsensusDivergedPage />} />
-          <Route path={NO_CONSENSUS_WHAT_NOW_ROUTE} element={<NoConsensusWhatNowPage />} />
           <Route path="*" element={<WelcomePage />} />
         </Routes>
       </AppLayout>
